c74c0455da0e1beb80f7d43f8f567d2b32b28842
pntp1986 <Baikiemtrathanh@gmail.com>
2025-11-04 11:45:41 +0700

    Add files via upload

--- js/loingo.js
+++ js/loingo.js
@@ -1,10 +1,9 @@
-// ===== TỰ ĐỘNG PHÁT NHẠC =====
 const music = document.getElementById("bgMusic");
 
-// 🔹 Khi trang được mở, kiểm tra nếu vừa bấm từ menu chính
+// 🔹 Nếu vừa bấm từ menu chính
 window.addEventListener("DOMContentLoaded", () => {
   if (localStorage.getItem("autoPlayLoingo") === "true") {
-    localStorage.removeItem("autoPlayLoingo"); // Xóa cờ sau khi dùng
+    localStorage.removeItem("autoPlayLoingo"); 
     music.play().catch(err => console.log("Autoplay bị chặn:", err));
   }
 });
