8c57e1e296ee17c6dbd7b17a91b1c21cc01a3eee
pntp1986 <Baikiemtrathanh@gmail.com>
2025-11-11 10:07:54 +0700

    Add files via upload

--- js/luubut.js
+++ js/luubut.js
@@ -20,31 +20,6 @@ if (localStorage.getItem("autoPlayLoingo") === "true") {
   }, 5000);
 }
 
-// Lấy các phần tử cần dùng
-const modal = document.getElementById("imageModal");
-const fullImage = document.getElementById("fullImage");
-const caption = document.getElementById("caption");
-
-// Khi người dùng bấm vào ảnh
-document.querySelectorAll(".clickable").forEach(img => {
-  img.addEventListener("click", () => {
-    modal.style.display = "block";
-    fullImage.src = img.src;
-    caption.innerText = img.alt;
-  });
-});
-
-// Nút đóng
-document.querySelector(".close").addEventListener("click", () => {
-  modal.style.display = "none";
-});
-
-// Bấm ra ngoài ảnh cũng đóng lại
-modal.addEventListener("click", (e) => {
-  if (e.target === modal) {
-    modal.style.display = "none";
-  }
-});
 
 // 🔹 Tự co chữ .fire-text vừa khung cha
 function scaleFireText() {
@@ -88,5 +63,11 @@ document.querySelectorAll('.clickable').forEach(el => {
 // Đóng modal
 document.querySelector('.close').addEventListener('click', () => {
   document.getElementById('imageModal').style.display = 'none';
+});
+
+// Bấm ra ngoài cũng đóng modal
+const modal = document.getElementById('imageModal');
+modal.addEventListener('click', (e) => {
+  if (e.target === modal) modal.style.display = 'none';
 });
 
